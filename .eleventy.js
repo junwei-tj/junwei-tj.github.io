d26623b509385e3d475192c9991b3fe694db7ca0
Jun Wei <tohjunwei.tj@gmail.com>
2022-07-03 21:13:17 +0800

    feat: add about page

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,12 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addLayoutAlias("page", "layouts/page.njk");
   eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
 
   eleventyConfig.addPlugin(syntaxHighlight);
+
+  eleventyConfig.addPassthroughCopy("./src/assets");
 
   eleventyConfig.addWatchTarget("./src/scss/");
 
