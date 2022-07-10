741a79a779cdce1c694b9156f78eb3a6a228e7f2
Jun Wei <tohjunwei.tj@gmail.com>
2022-07-10 11:29:31 +0800

    feat: add projects page

---
+++ src/_data/projects.js
@@ -0,0 +1,15 @@
+module.exports = [
+  {
+    title: "STARS Panel",
+    screenshot: "/assets/stars-panel.png",
+    description: `
+      STARS Panel is a course registration planner for NTU students. 
+      It aims to make timetable planning easy for students with features such as 
+      automatic timetable generation with special filters, as well as the editing 
+      and saving of timetable plans. Advanced options, such as eliminate back-to-back 
+      lessons, allow students to better plan their ideal timetables. 
+    `,
+    live: "https://stars-panel.netlify.app/",
+    github: "https://github.com/ruiofshens/ntu-stars-planner",
+  },
+];
