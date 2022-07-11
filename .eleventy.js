51ac1987cd62767f4c9714de81ac443e2610569f
Jun Wei <tohjunwei.tj@gmail.com>
2022-07-11 14:01:45 +0800

    feat: add blog list page

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,33 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const readingTime = require("eleventy-plugin-reading-time");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addLayoutAlias("page", "layouts/page.njk");
   eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
 
   eleventyConfig.addPlugin(syntaxHighlight);
+  eleventyConfig.addPlugin(readingTime);
+
+  eleventyConfig.addCollection("blogPosts", (collection) => {
+    return collection.getFilteredByGlob("./src/blog/*.md");
+  });
+
+  eleventyConfig.addFilter("formatDate", (dateObj) => {
+    const options = { month: "short", day: "numeric", year: "numeric" };
+    return Intl.DateTimeFormat("en-SG", options).format(dateObj);
+  });
+  eleventyConfig.addFilter("getTags", (tags) => {
+    const tagsCopy = [...tags];
+    tagsCopy.sort();
+    return tagsCopy;
+  });
 
   eleventyConfig.addPassthroughCopy("./src/assets");
+
+  eleventyConfig.setFrontMatterParsingOptions({
+    excerpt: true,
+    excerpt_separator: "<!-- excerpt -->",
+  });
 
   eleventyConfig.addWatchTarget("./src/scss/");
 
---
+++ src/blog/article5.md
@@ -0,0 +1,13 @@
+---
+title: Article 5
+date: 2022-01-01
+tags:
+  - tag99
+  - tag1
+---
+
+Article 3 Maecenas tristique elit eros, sed laoreet ligula sagittis eu. Donec consequat rutrum faucibus. Aliquam convallis nibh maximus neque rutrum, a aliquet odio iaculis.
+
+<!-- excerpt -->
+
+Duis eget sodales risus. Etiam in erat at sem auctor lobortis. Aliquam risus ante, suscipit ac porttitor eget, tristique non metus. Maecenas volutpat rhoncus urna, ac auctor purus eleifend a. Pellentesque viverra dictum erat, dictum sollicitudin sapien tristique mattis. Vivamus at feugiat nibh. Aliquam eu porta velit.
