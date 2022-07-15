e4f1426e5732005f05f72e29eaccfe1974dfb282
Jun Wei <tohjunwei.tj@gmail.com>
2022-07-16 00:15:04 +0800

    refactor: extract work experience as component

---
+++ src/_data/workExperiences.js
@@ -0,0 +1,24 @@
+module.exports = [
+  {
+    company: "Shopee",
+    logo: "/assets/shopee.svg",
+    logoAlt: "Logo of Shopee",
+    period: "May '22 - Aug '22",
+    title: "Software Engineer Intern",
+    description: [
+      "Handled the development of new features and fixing of bugs for Seller Center using <strong>Vue.js</strong>.",
+      "Implemented a new static site project using <strong>React.js</strong> and with custom <strong>Webpack 5</strong> configurations.",
+    ],
+  },
+  {
+    company: "Shopee",
+    logo: "/assets/sppl.svg",
+    logoAlt: "Logo of Singapore Pools Private Limited",
+    period: "May '21 - Jul '21",
+    title: "Testing and Automation Intern",
+    description: [
+      "Automated more than 50 manual test cases for both Android and iOS versions of the Singapore Pools app using <strong>Selenium</strong> and <strong>Appium</strong>.",
+      "Performed multiple regression testings and investigated when test cases failed.",
+    ],
+  },
+];
