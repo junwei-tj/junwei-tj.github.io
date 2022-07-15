56d65fd37ff1e45388ffa2e9d77c52d71569b88c
Jun Wei <tohjunwei.tj@gmail.com>
2022-07-15 23:15:16 +0800

    feat: add tags page

--- .eleventy.js
+++ .eleventy.js
@@ -9,20 +9,41 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPlugin(readingTime);
 
-  eleventyConfig.addCollection("blogPosts", (collection) => {
-    return collection.getFilteredByGlob("./src/blog/*.md").sort((a, b) => {
+  eleventyConfig.addCollection("blogPosts", (collections) => {
+    return collections.getFilteredByGlob("./src/blog/*.md").sort((a, b) => {
       return b.date - a.date;
     });
+  });
+  eleventyConfig.addCollection("blogTags", (collections) => {
+    const tagsSet = new Set();
+    const posts = collections.getFilteredByGlob("./src/blog/*.md");
+    posts.map((post) => {
+      if (post.data.tags) {
+        post.data.tags.map((tag) => tagsSet.add(tag));
+      }
+    });
+    const tags = Array.from(tagsSet);
+    tags.sort();
+    return tags;
   });
 
   eleventyConfig.addFilter("formatDate", (dateObj) => {
     const options = { month: "short", day: "numeric", year: "numeric" };
     return Intl.DateTimeFormat("en-SG", options).format(dateObj);
   });
+  eleventyConfig.addFilter("formatDateNumeric", (dateObj) => {
+    const options = { month: "numeric", day: "numeric", year: "numeric" };
+    return Intl.DateTimeFormat("en-SG", options)
+      .format(dateObj)
+      .replace(/\//g, "-");
+  });
   eleventyConfig.addFilter("getTags", (tags) => {
     const tagsCopy = [...tags];
     tagsCopy.sort();
     return tagsCopy;
+  });
+  eleventyConfig.addFilter("hasTag", (tags, tagToFind) => {
+    return tags && tags.includes(tagToFind);
   });
 
   eleventyConfig.addPassthroughCopy("./src/assets");
